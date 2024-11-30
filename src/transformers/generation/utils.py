e11d47078178be7adc8da0bdbb91ef35a061dd87
brian <brian030128@gmail.com>
2024-11-30 12:51:43 +0800

    print scores

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -3489,7 +3489,7 @@ class GenerationMixin:
                 next_token_logits, dim=-1
             )  # (batch_size * num_beams, vocab_size)
 
-            print(cur_len, "next scores ", next_token_scores)
+            
 
             next_token_scores_processed = logits_processor(input_ids, next_token_scores)
             next_token_scores = next_token_scores_processed + beam_scores[:, None].expand_as(
@@ -3552,6 +3552,8 @@ class GenerationMixin:
             beam_scores = beam_outputs["next_beam_scores"]
             beam_next_tokens = beam_outputs["next_beam_tokens"]
             beam_idx = beam_outputs["next_beam_indices"]
+
+            print(cur_len, "next scores ", beam_scores)
 
             input_ids = torch.cat([input_ids[beam_idx, :], beam_next_tokens.unsqueeze(-1)], dim=-1)
 
