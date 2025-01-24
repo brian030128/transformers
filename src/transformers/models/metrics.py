7bebbf9a359909f678b9964e50e3f4ff14fe6b3b
Sleep_AllDay <37238439+SpeedReach@users.noreply.github.com>
2025-01-24 13:39:58 +0800

    Update metrics.py

--- src/transformers/models/metrics.py
+++ src/transformers/models/metrics.py
@@ -9,12 +9,13 @@ def clear():
 
 import time
 import GPUtil
-
+import torch
 def get_gpu_usage():
     gpus = GPUtil.getGPUs()
     total = 0
     for gpu in gpus:
-        total += gpu.memoryUsed
+        total += (torch.cuda.memory_allocated(device=gpu.id) / 1000000) 
+
     return total
 
 
